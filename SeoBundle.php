e14853716304e6dc97d48c471cca11d1cb6272cc
Arthur <aguigand@umanit.fr>
2019-01-17 16:01:01 +0100

    Initial commit

---
+++ SeoBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Umanit\SeoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SeoBundle
+ *
+ * @author Arthur Guigand <aguigand@umanit.fr>
+ */
+class SeoBundle extends Bundle
+{
+}
