f02c735c1dc548013b1d27e5f128e14216dfdb62
Arthur <aguigand@umanit.fr>
2019-01-18 12:06:27 +0100

    Bootstrap unit tests.

---
+++ tests/Fixtures/App/AppKernel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Umanit\SeoBundle\Tests\App;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Umanit\SeoBundle\SeoBundle;
+
+/**
+ * The kernel used in the application of most functional tests.
+ */
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new DoctrineBundle(),
+            new SeoBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.yaml');
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return __DIR__.'/../../../build/cache/'.$this->getEnvironment();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return __DIR__.'/../../../build/kernel_logs/'.$this->getEnvironment();
+    }
+}
