4ac94d7692c858a17a0decf1b6af8c2d5e3bdb0a
Arthur <aguigand@umanit.fr>
2019-01-28 15:18:44 +0100

    Add SchemaOrgBuilder reading.

--- src/Model/AnnotationReaderTrait.php
+++ src/Model/AnnotationReaderTrait.php
@@ -2,8 +2,10 @@
 
 namespace Umanit\SeoBundle\Model;
 
+use Umanit\SeoBundle\Doctrine\Annotation\SchemaOrgBuilder;
 use Umanit\SeoBundle\Doctrine\Annotation\Seo;
 use Doctrine\Common\Annotations\Reader as AnnotationsReader;
+use Umanit\SeoBundle\Exception\NotSchemaOrgEntityException;
 use Umanit\SeoBundle\Exception\NotSeoEntityException;
 
 /**
@@ -50,6 +52,33 @@ trait AnnotationReaderTrait
 
         if (null === $seo) {
             throw new NotSeoEntityException(sprintf('Entity of type "%s" is not annotated by Seo(). Cannot use SEO features from it.', get_class($entity)));
+        }
+
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return $seo;
+    }
+
+    /**
+     * Returns the @SchemaOrg() annotation of an entity.
+     *
+     * @param      $entity
+     *
+     * @return SchemaOrgBuilder
+     * @throws NotSchemaOrgEntityException
+     * @throws \ReflectionException
+     */
+    public function getSchemaOrgAnnotation($entity): SchemaOrgBuilder
+    {
+        if (!is_object($entity)) {
+            throw new NotSchemaOrgEntityException(sprintf('A scalar cannot be annotated by SchemaOrg(). Cannot use generate schema from it.'));
+        }
+        $seo = $this->annotationsReader->getClassAnnotation(
+            new \ReflectionClass(get_class($entity)),
+            SchemaOrgBuilder::class
+        );
+
+        if (null === $seo) {
+            throw new NotSchemaOrgEntityException(sprintf('Entity of type "%s" is not annotated by SchemaOrg(). Cannot use generate schema from it.', get_class($entity)));
         }
 
         /** @noinspection PhpIncompatibleReturnTypeInspection */
