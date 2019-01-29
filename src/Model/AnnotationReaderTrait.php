b2c28cdc8086aecc3023e4c93ddffc586488ae55
Arthur <aguigand@umanit.fr>
2019-01-29 16:06:29 +0100

    Add breadcrumb to annotation reader

--- src/Model/AnnotationReaderTrait.php
+++ src/Model/AnnotationReaderTrait.php
@@ -3,9 +3,11 @@
 namespace Umanit\SeoBundle\Model;
 
 use Doctrine\ORM\Proxy\Proxy;
+use Umanit\SeoBundle\Doctrine\Annotation\Breadcrumb;
 use Umanit\SeoBundle\Doctrine\Annotation\SchemaOrgBuilder;
 use Umanit\SeoBundle\Doctrine\Annotation\Route;
 use Doctrine\Common\Annotations\Reader as AnnotationsReader;
+use Umanit\SeoBundle\Exception\NotBreadcrumbEntityException;
 use Umanit\SeoBundle\Exception\NotSchemaOrgEntityException;
 use Umanit\SeoBundle\Exception\NotSeoRouteEntityException;
 
@@ -44,7 +46,7 @@ trait AnnotationReaderTrait
     public function getSeoRouteAnnotation($entity): Route
     {
         if (!is_object($entity)) {
-            throw new NotSeoRouteEntityException(sprintf('A scalar cannot be annotated by Seo\Route(). Cannot use Seo routing features from it.'));
+            throw new NotSeoRouteEntityException(sprintf('Value "%s" is a scalar, it cannot be annotated by Seo\Route(). Cannot use Seo routing features from it.', (string) $entity));
         }
 
         $route = $this->annotationsReader->getClassAnnotation(
@@ -72,7 +74,7 @@ trait AnnotationReaderTrait
     public function getSchemaOrgAnnotation($entity): SchemaOrgBuilder
     {
         if (!is_object($entity)) {
-            throw new NotSchemaOrgEntityException(sprintf('A scalar cannot be annotated by SchemaOrg(). Cannot use generate schema from it.'));
+            throw new NotSchemaOrgEntityException(sprintf('Value "%s" is a scalar, it cannot be annotated by SchemaOrg(). Cannot use generate schema from it.', (string) $entity));
         }
 
         $schemaOrgBuilder = $this->annotationsReader->getClassAnnotation(
@@ -81,11 +83,39 @@ trait AnnotationReaderTrait
         );
 
         if (null === $schemaOrgBuilder) {
-            throw new NotSchemaOrgEntityException(sprintf('Entity of type "%s" is not annotated by SchemaOrg(). Cannot use generate schema from it.', get_class($entity)));
+            throw new NotSchemaOrgEntityException(sprintf('Entity of type "%s" is not annotated by SchemaOrg(). Cannot generate schema from it.', get_class($entity)));
         }
 
         /** @noinspection PhpIncompatibleReturnTypeInspection */
         return $schemaOrgBuilder;
+    }
+
+    /**
+     * Returns the @Breadcrumb() annotation of an entity.
+     *
+     * @param $entity
+     *
+     * @return Breadcrumb
+     * @throws NotBreadcrumbEntityException
+     * @throws \ReflectionException
+     */
+    public function getBreadcrumbAnnotation($entity): Breadcrumb
+    {
+        if (!is_object($entity)) {
+            throw new NotBreadcrumbEntityException(sprintf('Value "%s" is a scalar, it cannot be annotated by Breadcrumb(). Cannot generate breadcrumb from it.', (string) $entity));
+        }
+
+        $breadcrumb = $this->annotationsReader->getClassAnnotation(
+            new \ReflectionClass($this->getClass($entity)),
+            Breadcrumb::class
+        );
+
+        if (null === $breadcrumb) {
+            throw new NotBreadcrumbEntityException(sprintf('Entity of type "%s" is not annotated by Breadcrumb(). Cannot generate breadcrumb from it.', get_class($entity)));
+        }
+
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return $breadcrumb;
     }
 
     /**
