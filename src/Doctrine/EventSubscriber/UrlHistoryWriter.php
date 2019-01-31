a9c36eed97bae100e710e32435552b2f90bda2bb
Arthur <aguigand@umanit.fr>
2019-01-31 15:17:19 +0100

    Remove dead code.

--- src/Doctrine/EventSubscriber/UrlHistoryWriter.php
+++ src/Doctrine/EventSubscriber/UrlHistoryWriter.php
@@ -97,39 +97,6 @@ class UrlHistoryWriter implements EventSubscriber
         } catch (NotSeoRouteEntityException $e) {
             // Do nothing
         }
-
-        // Generate history for entities associated to this very entity.
-        // Loop through all existing entities
-        $metas = $args->getEntityManager()->getMetadataFactory()->getAllMetadata();
-        foreach ($metas as $meta) {
-            $class           = $meta->getName();
-            $reflectionClass = new \ReflectionClass($class);
-            foreach ($reflectionClass->getProperties() as $property) {
-                $reader = new AnnotationReader();
-                /** @var ManyToOne $manyToOne */
-                $manyToOne = $reader->getPropertyAnnotation($property, ManyToOne::class);
-                // Search if an attribute is a ManyToOne associated to the current updated entity class
-                if (null === $manyToOne || $manyToOne->targetEntity !== $this->getClass($entity)) {
-                    continue;
-                }
-                // If so, fetch all entities
-                $entities = $args->getEntityManager()->getRepository($class)->findBy([$property->name => $entity]);
-                foreach ($entities as $subEntity) {
-                    // Build the new path
-                    $subNewPath = $this->urlBuilder->path($subEntity);
-                    // Get old values
-                    $subChangeSet = [];
-                    foreach ($changeSet as $changeFieldKey => $changedFieldValue) {
-                        $subChangeSet[$property->name.'.'.$changeFieldKey] = $changedFieldValue;
-                    }
-                    $subOldPath = $this->urlBuilder->path($subEntity, $subChangeSet);
-
-                    // Add it to the pool
-                    $this->urlPool->add($subOldPath, $subNewPath, $subEntity);
-                }
-            }
-
-        }
     }
 
     public function postFlush(PostFlushEventArgs $args)
@@ -140,19 +107,5 @@ class UrlHistoryWriter implements EventSubscriber
     public function postRemove(LifecycleEventArgs $args)
     {
         // TODO
-    }
-
-    /**
-     * Returns the entity class.
-     *
-     * @param object $entity
-     *
-     * @return string
-     */
-    private function getClass(object $entity): string
-    {
-        return ($entity instanceof Proxy)
-            ? get_parent_class($entity)
-            : get_class($entity);
     }
 }
