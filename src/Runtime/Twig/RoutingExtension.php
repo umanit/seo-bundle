3c918f6729e702bae7eb4a177d8f8e9d4cf5783a
DjLeChuck <djlechuck@gmail.com>
2021-08-02 21:05:19 +0200

    Prepare code for PHP 8.0
    - Fix non static call of isUrlGenerationSafe in RoutingExtension

--- src/Runtime/Twig/RoutingExtension.php
+++ src/Runtime/Twig/RoutingExtension.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Umanit\SeoBundle\Runtime\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\Node\Expression\ArrayExpression;
+use Twig\Node\Expression\ConstantExpression;
+use Twig\Node\Node;
 use Twig\TwigFunction;
 
 class RoutingExtension extends AbstractExtension
@@ -15,13 +18,37 @@ class RoutingExtension extends AbstractExtension
             new TwigFunction(
                 'url',
                 [RoutingRuntime::class, 'getUrl'],
-                ['is_safe_callback' => [RoutingRuntime::class, 'isUrlGenerationSafe']]
+                ['is_safe_callback' => [$this, 'isUrlGenerationSafe']]
             ),
             new TwigFunction(
                 'path',
                 [RoutingRuntime::class, 'getPath'],
-                ['is_safe_callback' => [RoutingRuntime::class, 'isUrlGenerationSafe']]
+                ['is_safe_callback' => [$this, 'isUrlGenerationSafe']]
             ),
         ];
+    }
+
+    /**
+     * @param Node $argsNode
+     *
+     * @return array
+     *
+     * @see \Symfony\Bridge\Twig\Extension\RoutingExtension::isUrlGenerationSafe
+     */
+    public function isUrlGenerationSafe(Node $argsNode): array
+    {
+        // support named arguments
+        $paramsNode = $argsNode->hasNode('parameters') ?
+            $argsNode->getNode('parameters') :
+            ($argsNode->hasNode('1') ? $argsNode->getNode('1') : null);
+
+        if (
+            null === $paramsNode || $paramsNode instanceof ArrayExpression && \count($paramsNode) <= 2 &&
+            (!$paramsNode->hasNode('1') || $paramsNode->getNode('1') instanceof ConstantExpression)
+        ) {
+            return ['html'];
+        }
+
+        return [];
     }
 }
--- src/Runtime/Twig/RoutingRuntime.php
+++ src/Runtime/Twig/RoutingRuntime.php
@@ -6,9 +6,6 @@ namespace Umanit\SeoBundle\Runtime\Twig;
 
 use Symfony\Bridge\Twig\Extension\RoutingExtension as BaseRoutingExtension;
 use Twig\Extension\RuntimeExtensionInterface;
-use Twig\Node\Expression\ArrayExpression;
-use Twig\Node\Expression\ConstantExpression;
-use Twig\Node\Node;
 use Umanit\SeoBundle\Model\RoutableModelInterface;
 use Umanit\SeoBundle\Routing\Canonical;
 
@@ -50,29 +47,5 @@ class RoutingRuntime implements RuntimeExtensionInterface
         }
 
         return $this->decorated->getPath($name, $parameters, $schemeRelative);
-    }
-
-    /**
-     * @param Node $argsNode
-     *
-     * @return array
-     *
-     * @see \Symfony\Bridge\Twig\Extension\RoutingExtension::isUrlGenerationSafe
-     */
-    public function isUrlGenerationSafe(Node $argsNode): array
-    {
-        // support named arguments
-        $paramsNode = $argsNode->hasNode('parameters') ?
-            $argsNode->getNode('parameters') :
-            ($argsNode->hasNode('1') ? $argsNode->getNode('1') : null);
-
-        if (
-            null === $paramsNode || $paramsNode instanceof ArrayExpression && \count($paramsNode) <= 2 &&
-            (!$paramsNode->hasNode('1') || $paramsNode->getNode('1') instanceof ConstantExpression)
-        ) {
-            return ['html'];
-        }
-
-        return [];
     }
 }
